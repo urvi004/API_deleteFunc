3bd3808bcc2f9a45594d0760ee3f94bb20b02cec
urvi004 <urvisharma54@gmail.com>
2022-04-19 12:20:51 +0530

    Updates

---
+++ precache-manifest.03644f2fb356274440e8c32d7cb30d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c17d259a5b60b32e4da9",
+    "url": "/fagroWiz/static/css/main.285e3f12.chunk.css"
+  },
+  {
+    "revision": "c17d259a5b60b32e4da9",
+    "url": "/fagroWiz/static/js/main.c17d259a.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/fagroWiz/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "10992e196e3dd19b20c1",
+    "url": "/fagroWiz/static/js/2.10992e19.chunk.js"
+  },
+  {
+    "revision": "278dd6a10eac494d3b5a",
+    "url": "/fagroWiz/static/js/runtime~main.278dd6a1.js"
+  },
+  {
+    "revision": "2f9c9821c1d9167e5ece079ed24e905f",
+    "url": "/fagroWiz/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fagroWiz/precache-manifest.03644f2fb356274440e8c32d7cb30d32.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fagroWiz/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
