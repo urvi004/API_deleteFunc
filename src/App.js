682d9d49ed90b8c85260b8cba9a2b3264a26872d
urvi004 <urvisharma54@gmail.com>
2022-04-19 16:04:11 +0530

    delete func added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
-  return (
+function App(){
+  const [users,setUsers]= useState([])
+  useEffect(() => {
+    getUsers()
+  },[])
+
+  function getUsers()
+    {
+    
+      fetch("https://jsonplaceholder.typicode.com/posts").then((result) =>{
+        result.json().then((resp)=>{
+          //console.warn("result",resp)
+          setUsers(resp)
+        })
+      })
+    }
+  
+
+
+  console.warn(users)
+
+  function deleteUser(id){
+    fetch(`https://jsonplaceholder.typicode.com/posts ${id}`,{
+      method: "DELETE"
+    }).then((result)=>{
+      result.json().then((resp)=>{
+        console.warn(resp)
+        getUsers()
+       
+      })
+    })
+    }
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>users data</h1>
+      <table border = "1">
+        <tbody>
+        <tr>
+          <td>userId</td>
+          <td>ID</td>
+          <td>Title</td>
+          <td>Body</td>
+          <td>operations</td>
+        </tr>
+        {
+          users.map((item,i)=>
+            <tr key={i}>
+          <td>{ item.userId }</td>
+          <td>{ item.id }</td>
+          <td>{ item.title }</td>
+          <td>{ item.body }</td>
+          <td><button onClick={()=>deleteUser(item.id)}>Delete</button></td>
+        </tr>
+
+          )
+        }
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
+//hi
+
+
 
 export default App;
+
+
+
+
